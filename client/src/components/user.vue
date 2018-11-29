fa509ed7e787f0176ebdcbd005e48acea5baa97e
Benedict Singer <benedict.singer@automattic.com>
2018-11-29 09:51:47 -0500

    Refactor the pill component out

--- client/src/components/user.vue
+++ client/src/components/user.vue
@@ -1,22 +1,20 @@
 <template>
-	<span class='userName'>{{ user.name }}</span>
+	<wl-pill :color='"lightsalmon"'>{{ user.name }}</wl-pill>
 </template>
 
 <style lang="scss">
-
-span.userName {
-	background-color: lightsalmon;
-	border-radius: 0.5em;
-	padding: 0.25em;
-	margin: 0.25em;
-}
 
 </style>
 
 <script lang="ts">
 import Vue from 'vue';
 
+import Pill from './pill.vue';
+
 export default Vue.extend( {
 	props: [ 'user' ],
+	components: {
+		'wl-pill': Pill,
+	},
 } );
 </script>
